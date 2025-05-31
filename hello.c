37a533c952884eb7f244a10856e08a0bfeaf6c05
Devin Zimmer <dz@renttherunway.com>
2025-05-31 07:45:52 -0400

    feat: hello world with some vars

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 45;
+    float weight = 99.5;
+    char grade = 'A';
+
+    printf("Hello, Devin!\n");
+    printf("Aged %d\n", age);
+    printf("Weight %.1f kg.\n", weight);
+    printf("Grade: %c\n", grade);
+    return 0;
+}
+
