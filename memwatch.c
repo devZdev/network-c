fe92e9a1082dace67083adb7c6efb1d797dd1f3a
Devin Zimmer <dz@renttherunway.com>
2025-06-07 22:11:00 -0400

    feat: allocate and free bytes, track the bytes, report on usage

--- memwatch.c
+++ memwatch.c
@@ -4,7 +4,43 @@
 #include <unistd.h>
 
 int main() {
-    char *buffer = malloc(128);
-    strcpy(buffer, "This memory was manually allocated");
-    write(1, buffer, 128);
+    size_t buffer_size_a = 128;
+    size_t buffer_size_b = 64;
+    size_t total_allocated = 0;
+    size_t total_freed = 0;
+
+
+    char *a_buffer = malloc(buffer_size_a);
+    char *b_buffer = malloc(buffer_size_b);
+
+    if (a_buffer == NULL || b_buffer == NULL) {
+        fprintf(stderr, "No memory allocated, exiting!");
+        return 1;
+    }
+
+    strcpy(a_buffer, "\nThis memory was manually allocated to buffer a");
+    total_allocated += buffer_size_a;
+
+    strcpy(b_buffer, "\nThis memory was manually allocated to buffer b");
+    total_allocated += buffer_size_b;
+
+    printf(
+        "\nWe allocated %zd bytes of memory",
+        total_allocated
+    );
+
+    write(1, a_buffer, strlen(a_buffer));
+    write(1, b_buffer, strlen(b_buffer));
+
+    free(a_buffer);
+    total_freed += buffer_size_a;
+
+    free(b_buffer);
+    total_freed += buffer_size_b;
+
+    printf(
+        "\nWe freed %zd bytes of memory" ,
+        total_freed
+    );
+    return 0;
 }
