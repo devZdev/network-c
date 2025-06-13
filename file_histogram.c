8be3a88343d370f0658a68fa80fcb61b9341aebe
Devin Zimmer <dz@renttherunway.com>
2025-06-13 14:55:26 -0400

    chore: cleanup

--- file_histogram.c
+++ file_histogram.c
@@ -3,25 +3,14 @@
 #include <unistd.h>
 
 int main() {
-    /*
-    * f
-* Open a file
-* Create a buffer
-* Read the file into a buffer
-* Create an integer data structure where each byte is initialized with zero
-* read the buffer with the file contents and print out histogram style
-*
-*
-*
-* */
     int fd = open("test.txt", O_RDONLY);
-    char buffer[64];
+    unsigned char buffer[64];
     ssize_t bytes_read;
     size_t histogram[256] = {0};
 
     while((bytes_read = read(fd, buffer, sizeof(buffer)))){
         for(size_t i = 0; i < bytes_read; i++) {
-            histogram[(unsigned char)buffer[i]]++;
+            histogram[buffer[i]]++;
         };
     };
 
